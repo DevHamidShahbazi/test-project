b4a9b659367a445d255865c4294f43434f65ffac
DevHamidShahbazi <dev.hamidshahbazi@gmail.com>
2023-12-06 20:10:44 +0330

    cleanup

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -2,4 +2,17 @@
 @section('dashboard','active')
 @section('content')
     <h1 class="text-center p-5 font-weight-bold">پنل مدیریت</h1>
+    <div class="col-12">
+
+        <div class="card">
+            <p class="text-center">لیست محصولات</p>
+            <b class="text-center">method:GET   http://fanoram.ir/products</b>
+        </div>
+
+        <div class="card">
+            <p class="text-center">ثبت سفارش (برای دسترسی باید ثبت نام کنید)</p>
+            <b class="text-center">method:POST   http://fanoram.ir/store-order</b>
+        </div>
+
+    </div>
 @endsection
