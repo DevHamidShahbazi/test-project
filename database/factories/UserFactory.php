dd70cb3f65de90e6f93745f16e6a0a26a7691f89
DevHamidShahbazi <dev.hamidshahbazi@gmail.com>
2023-12-06 11:06:36 +0330

    add controller login

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'phone' => fake()->unique()->phoneNumber(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
